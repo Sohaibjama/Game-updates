4d6c13828e93f303cffeb67a8a4a54fce48f8aec
Sohaibjama <sohaibjamal077@gmail.com>
2025-09-19 18:55:22 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Ad link (always same)
-const adLink = "https://virtualflashy.com/mta44zsw?key=c3759a44256c5bfc7ab104a93478c66e";
+const adLink ="https://virtualflashy.com/py50wtx4?key=9629d5c2057e2d27f9c8dd31f5f90e22";
 
 // Mapping buttons → target pages
 const buttonRedirects = {
@@ -26,4 +26,25 @@ Object.keys(buttonRedirects).forEach(btnId => {
       }, 1000);
     });
   }
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll("article, p, h2, h3, h4");
+  
+  // hide them first
+  elements.forEach(el => {
+    el.style.opacity = "0";
+    el.style.transform = "translateY(20px)";
+    el.style.transition = "opacity 0.8s ease-out, transform 0.8s ease-out";
+  });
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  }, { threshold: 0.1 });
+  
+  elements.forEach(el => observer.observe(el));
 });
